c4ee289d2944256eae19fbed3147ccd47a911aa0
Dray Williams <26555614+DrayWilliams1@users.noreply.github.com>
2021-01-21 12:47:46 -0500

    Patient Filtering + App Completion
    - FInished up final touches regarding data filtering and polish

--- src/app/protected/patientattributes/patientattributes.page.ts
+++ src/app/protected/patientattributes/patientattributes.page.ts
@@ -9,7 +9,11 @@ import { PatientService } from 'src/app/services/patient.service';
   styleUrls: ['./patientattributes.page.scss'],
 })
 export class PatientattributesPage implements OnInit {
-  private patients: any // all database patients
+  private patients: any = []              // all database patients
+  public filteredPatients: any = []      // a subset of filtered patients based on the original data
+  private filterTerm: string              // the criteria to filter the results
+  private filterValue: string             // the value to filter the reults
+  public covidSelector: boolean = false;  // a flag indicating which search field to use
 
   constructor(
     private location: Location,
@@ -18,10 +22,11 @@ export class PatientattributesPage implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.patientService.getAllPatients().subscribe((data) => {
+    this.patientService.getAllPatients().subscribe((data) => { // obtains all patients from the database
       console.log(data)
 
       this.patients = data
+      this.filteredPatients = data
     })
   }
 
@@ -36,4 +41,55 @@ export class PatientattributesPage implements OnInit {
     }
   }
 
+  /**
+   * Updates the filter criteria based on the filter select field.
+   * Sets covid flag based on the filter criteria.
+   * 
+   * @param $event the event trigerring this function
+   */
+  getFilterTerm($event): void {
+    let term: string = $event.target.value
+
+    if(term == 'hasCovid') { // updates the covid selector flag to change the search input to a true/false selector
+      this.covidSelector = true
+    } else {
+      this.covidSelector = false
+    }
+
+    this.filterTerm = term
+  }
+
+  /**
+   * Updates the filter value based on the search bar input
+   * 
+   * @param $event the event trigerring this function
+   */
+  getFilterValue($event): void {
+    this.filterValue = $event.target.value;
+  }
+
+  /**
+   * Filters the list of patient data based on search criteria and value
+   */
+  filterPatientData(): void {
+    if (this.filterTerm == null || this.filterValue == null) { // if either of the search fields are empty, return the full list of patients
+      this.filteredPatients = this.patients
+    } else {
+      this.filteredPatients = this.patients.filter((patient) => { // filters based on different data properties based on the selected filter criteria
+        if(this.filterTerm == 'firstName') {
+          return (
+            patient.firstName.toLowerCase().indexOf(this.filterValue.toLowerCase()) > -1
+          )
+        } else if (this.filterTerm == 'postalCode') {
+          return (
+            patient.postal_Code.toLowerCase().indexOf(this.filterValue.toLowerCase()) > -1
+          )
+        } else if (this.filterTerm == 'hasCovid') {
+          return (
+            String(patient.hasCovid).indexOf(this.filterValue) > -1
+          )
+        }
+      })
+    }
+  }
 }
--- src/app/protected/patientdates/patientdates.page.ts
+++ src/app/protected/patientdates/patientdates.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
+import { PatientService } from 'src/app/services/patient.service';
 
 @Component({
   selector: 'app-patientdates',
@@ -9,12 +10,24 @@ import { Location } from '@angular/common';
 })
 export class PatientdatesPage implements OnInit {
 
+  private patients: any = []              // all database patients
+  public filteredPatients: any = []      // a subset of filtered patients based on the original data
+  private filterTerm: string              // the criteria to filter the results
+  private filterValue: string             // the value to filter the reults
+
   constructor(
     private location: Location,
-    private router: Router
+    private router: Router,
+    private patientService: PatientService
   ) { }
 
   ngOnInit() {
+    this.patientService.getAllPatients().subscribe((data) => { // obtains all patients from the database
+      console.log(data)
+
+      this.patients = data
+      this.filteredPatients = data
+    })
   }
 
   /**
@@ -25,6 +38,62 @@ export class PatientdatesPage implements OnInit {
       this.location.back()
     } else { // no history
       this.router.navigate(['/superdashboard'])
+    }
+  }
+
+  /**
+   * Updates the filter criteria based on the filter select field
+   * 
+   * @param $event the event trigerring this function
+   */
+  getFilterTerm($event): void {
+    console.log($event.target.value)
+    let term: string = $event.target.value
+
+    this.filterTerm = term
+  }
+
+  /**
+   * Updates the filter value based on the search bar input
+   * 
+   * @param $event the event trigerring this function
+   */
+  getFilterValue($event): void {
+    let date: Date = new Date($event.target.value)
+    let formatDate: string = date.getFullYear() + '-' + this.addZeroes(date.getMonth()+1) + '-' + this.addZeroes(date.getDate())
+    console.log(formatDate)
+    this.filterValue = formatDate
+  }
+
+  /**
+   * Adds zeroes to single digit dates. Useful for when the returned day of the month
+   * or month of the year is less then 10. Eg. 09/06/2020
+   * 
+   * @param num the number to be altered (if a single digit)
+   * 
+   * @returns the (possible) altered number
+   */
+  addZeroes(num: number): string {
+    return num < 10? '0'+ num : '' + num;
+  }
+
+  filterPatientData(): void {
+    console.log(this.filterValue)
+    if (this.filterTerm == null || this.filterValue == null) {
+      this.filteredPatients = this.patients
+    } else {
+      this.filteredPatients = this.patients.filter((patient) => { // filters based on different data properties based on the selected filter criteria
+        if(this.filterTerm == 'birthDate') {
+          
+          return (
+            patient.birthDate.toLowerCase().indexOf(this.filterValue.toLowerCase()) > -1
+          )
+        } else if (this.filterTerm == 'call_Date') {
+          return (
+            patient.call_Date.toLowerCase().indexOf(this.filterValue.toLowerCase()) > -1
+          )
+        }
+      })
     }
   }
 
